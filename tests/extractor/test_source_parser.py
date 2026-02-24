6bb5405b06f9a90645da83a120462cb5260e62aa
Marcuth <thiago.playgames15@gmail.com>
2026-02-24 03:04:55 -0300

    tests: add tests for SourceParser

---
+++ tests/extractor/test_source_parser.py
@@ -0,0 +1,19 @@
+from xcrap.extractor import SourceParser
+from tests.extractor.test_html_extraction_model import parser
+
+def test_source_parser_should_defined() -> None:
+    assert SourceParser
+    
+def test_init_source_parser() -> None:
+    input = "xpto"
+    parser = SourceParser(input)
+    
+    assert parser.content == input
+    
+def test_init_with_file_loading(tmp_path: str) -> None:
+    file_path = tmp_path / "example.txt"
+    file_path.write_text("xpto")
+
+    parser = SourceParser.load_file(str(file_path))
+
+    assert parser.content == "xpto"
