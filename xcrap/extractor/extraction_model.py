a5d2ea763fb6e5a9aacce201b5281f902bb41100
Marcuth <thiago.playgames15@gmail.com>
2026-03-12 00:07:15 -0300

    docs: add docstring to ExtractionModel base class

--- xcrap/extractor/extraction_model.py
+++ xcrap/extractor/extraction_model.py
@@ -2,6 +2,9 @@ from abc import ABC, abstractmethod
 
 
 class ExtractionModel(ABC):
+    """
+    Abstract base class for all extraction models.
+    """
     @abstractmethod
     def extract(self, content: str) -> any:
         pass
